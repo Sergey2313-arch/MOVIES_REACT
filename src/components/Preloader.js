b41fa17ac070ba8c99feecf55c6ee6b058a926de
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-28 16:29:16 +0300

    Add preloader component

---
+++ src/components/Preloader.js
@@ -0,0 +1,15 @@
+import './Preloader.css';
+
+function Preloader() {
+  return (
+    <div className="preloader-wrap">
+      <div className="lds-facebook">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Preloader;
