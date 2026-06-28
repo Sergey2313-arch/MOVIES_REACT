b3005ac20c2148bb31f10af827ab033221a90ab3
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-28 16:27:59 +0300

    Add footer component

---
+++ src/layout/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>React Movies App &copy; {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
