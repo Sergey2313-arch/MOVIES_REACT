b5519697865c8a98710296903fa3604d981547ab
Sergey2313-arch <skorobkov435@gmail.com>
2026-06-28 16:25:07 +0300

    Add header component

---
+++ src/layout/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Video</h1>
+      <div className="contacts">
+        <p>Movie search application</p>
+        <p>OMDb API</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
